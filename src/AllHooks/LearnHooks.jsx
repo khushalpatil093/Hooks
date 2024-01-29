8a43121f738b8c440eefc6bbd33b5e3c8bd27e45
khushalpatil093 <khushalpatil093@gmail.com>
2024-01-29 22:18:04 +0530

    done

--- src/AllHooks/LearnHooks.jsx
+++ src/AllHooks/LearnHooks.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/react.svg"
 
 const LearnHooks = () => {
   return (
     <div>
+      <div>
+        <img  style={{height: "10em"}} src={Logo} alt="React logo" />
         <h1>Let's Learn React Hooks</h1>
+      </div>
+      <div>
+        <p style={{ fontWeight: "bold" }}>
+          Hooks let you use different React features from your components. You
+          can either use the built-in Hooks or combine them to build your own.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LearnHooks;
